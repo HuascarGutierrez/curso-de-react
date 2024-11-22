65b8af5f71cb8effe8d7c16d19bff45251086ba6
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2024-11-22 17:54:15 -0400

    functionally implemented

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -30,7 +30,7 @@ const WINNER_COMBOS = [
 function App() {
 
   const updateBoard = (index) => {
-    if(board[index] === null)
+    if(board[index] === null && winner===null)
     {
       const newBoard = [...board]
       newBoard[index] = turn
@@ -40,24 +40,37 @@ function App() {
       setTurn(newTurn)
 
       const newWinner = checkWinner(newBoard)
-      if (newWinner) {
+      if (newWinner || newWinner === false) {
         setWinner(newWinner)
-        console.log(newWinner)
+        //console.log(`${newWinner} wins. WINNER WINNER CHICKEN DINNER`)
+        //como los setters en react son asincornos, no impiden la ejecucion del resto del codigo mientras se cambia el valor de la variable 
       }
     }
   } 
 
   const checkWinner = (boardToCheck) => {
+    const playCount = plays + 1
+    setPlays(playCount)
+    console.log(playCount)
     for (const combo of WINNER_COMBOS) {
       const [a,b,c] = combo
       if(boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) return boardToCheck[a]
     }
+    if (playCount === 9 ) return false
     return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(turns.X)
+    setWinner(null)
+    setPlays(0)
   }
 
 const [board, setBoard] = useState(Array(9).fill(null))
 const [turn, setTurn] = useState(turns.X)
 const [winner, setWinner] = useState(null) //para este caso, null = no winner, false = draw
+const [plays,setPlays] = useState(0)
 
   return (
     <>
@@ -76,6 +89,32 @@ const [winner, setWinner] = useState(null) //para este caso, null = no winner, f
         <Square isSelected={turn === turns.X}>{turns.X}</Square>
         <Square isSelected={turn === turns.O}>{turns.O}</Square>
 
+      </section>
+
+      <section>
+        {
+          winner !== null && (
+            <section className='winner'>
+              <div className='text'>
+                <h2>
+                  {
+                    winner === false ?
+                      'EMPATE':
+                      'THIS GUY IS THE WINNER'
+                  }
+                </h2>
+
+                <header>
+                  {winner && <Square>{winner}</Square>}
+                </header>
+
+                <footer>
+                  <button onClick={resetGame}>Start again</button>
+                </footer>
+              </div>
+            </section>
+          )
+        }
       </section>
     </main>
     </>
