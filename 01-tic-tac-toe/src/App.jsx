880b9b33d10d86314a0629d40f612df24a7ad220
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2024-12-03 14:19:43 -0400

    git ignores now

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import confetti from 'canvas-confetti'
 import Square from './Square'
 import { turns, WINNER_COMBOS } from './components'
@@ -25,6 +25,8 @@ function App() {
       if (newWinner || newWinner === false) {
         if(newWinner !== false) confetti()
         setWinner(newWinner)
+
+        window.localStorage.setItem('winner',JSON.stringify(newWinner))
         //console.log(`${newWinner} wins. WINNER WINNER CHICKEN DINNER`)
         //como los setters en react son asincornos, no impiden la ejecucion del resto del codigo mientras se cambia el valor de la variable 
       }
@@ -34,6 +36,9 @@ function App() {
   const checkWinner = (boardToCheck) => {
     const playCount = plays + 1
     setPlays(playCount)
+
+    window.localStorage.setItem('plays', playCount)  
+
     for (const combo of WINNER_COMBOS) {
       const [a,b,c] = combo
       if(boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) return boardToCheck[a]
@@ -47,12 +52,39 @@ function App() {
     setTurn(turns.X)
     setWinner(null)
     setPlays(0)
+    window.localStorage.setItem('board', JSON.stringify(Array(9).fill(null)))
+    window.localStorage.setItem('turn', JSON.stringify(turns.X))
+    window.localStorage.setItem('plays', JSON.stringify(0))
+    window.localStorage.setItem('winner', JSON.stringify(null))
   }
 
-const [board, setBoard] = useState(Array(9).fill(null))
-const [turn, setTurn] = useState(turns.X)
-const [winner, setWinner] = useState(null) //para este caso, null = no winner, false = draw
-const [plays,setPlays] = useState(0)
+const [board, setBoard] = useState( () => {
+  const boardFromLocalStorago = window.localStorage.getItem('board')
+  return boardFromLocalStorago ? JSON.parse(boardFromLocalStorago) : 
+  Array(9).fill(null)
+}
+)
+const [turn, setTurn] = useState( ()=> {
+  const turnFromLocalStorage = window.localStorage.getItem('turn')
+  return turnFromLocalStorage ? JSON.parse(turnFromLocalStorage):
+  turns.X
+})
+const [winner, setWinner] = useState(() => {
+  const winnerFromLocal = window.localStorage.getItem('winner')
+  return winnerFromLocal ? JSON.parse(winnerFromLocal): null
+}) //para este caso, null = no winner, false = draw
+
+const [plays,setPlays] = useState( () => {
+  const plays = window.localStorage.getItem('plays')
+  return plays ? plays : 0
+}
+
+)
+
+useEffect( () => {
+  console.log('usando el useEffect')
+}, [winner])
+
 
   return (
     <>
