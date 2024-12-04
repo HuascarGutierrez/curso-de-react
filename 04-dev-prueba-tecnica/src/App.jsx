4a84bf5dd174f02da471406e9cdb03e4519e57cf
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2024-12-04 17:05:46 -0400

    useCatImage

--- 04-dev-prueba-tecnica/src/App.jsx
+++ 04-dev-prueba-tecnica/src/App.jsx
@@ -3,12 +3,23 @@ import { getRandomFact } from './services/facts'
 
 import './App.css'
 
+function useCatImage ({fact}) {
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    if(!fact) return
+    
+    const words = fact
+    const firstWord = words.split(' ')[0]
+    setImageUrl(`https://cataas.com/cat/says/${firstWord}?width=300&height=300&fontSize=32&fontColor=white`)
+  },[fact])
+  return {imageUrl}
+} 
+
 //const CAT_ENDPOINT_RANDOM_IMAGE = `https://cataas.com/cat/says/${firstWord}?size=50&color-red&json=true`
 function App () {
   const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
-
-
+  const {imageUrl} = useCatImage({fact})
   
   useEffect( () => {
     async function setRandomFact() {
@@ -19,13 +30,7 @@ function App () {
   }
   , [])
 
-  useEffect(() => {
-    if(!fact) return
-    
-    const words = fact
-    const firstWord = words.split(' ')[0]
-    setImageUrl(`https://cataas.com/cat/says/${firstWord}?width=300&height=300&fontSize=32&fontColor=white`)
-  },[fact])
+  
 
   const handleClick = async() => {
     const newFact = await getRandomFact()
