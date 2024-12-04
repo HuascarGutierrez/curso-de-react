9355eff5c15daa95ab3bdc1eab6b7a691987a62c
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2024-12-04 16:29:04 -0400

    async and await

--- 04-dev-prueba-tecnica/src/App.jsx
+++ 04-dev-prueba-tecnica/src/App.jsx
@@ -1,26 +1,41 @@
 import React, { useEffect, useState } from 'react'
+import { getRandomFact } from './services/facts'
+
 import './App.css'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 //const CAT_ENDPOINT_RANDOM_IMAGE = `https://cataas.com/cat/says/${firstWord}?size=50&color-red&json=true`
 function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
 
-        const firstWord = fact.split(' ')[0]
-        setImageUrl(`https://cataas.com/cat/says/${firstWord}?width=200&height=200&fontSize=32&fontColor=white`)
-      })
-  }, [])
+
+  
+  useEffect( () => {
+    async function setRandomFact() {
+      const newFact = await getRandomFact()
+      setFact(newFact)
+      }
+    setRandomFact();
+  }
+  , [])
+
+  useEffect(() => {
+    if(!fact) return
+    
+    const words = fact
+    const firstWord = words.split(' ')[0]
+    setImageUrl(`https://cataas.com/cat/says/${firstWord}?width=300&height=300&fontSize=32&fontColor=white`)
+  },[fact])
+
+  const handleClick = async() => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
 
   return (
     <main className='card'>
       <h1 className='title'>App de gatitos</h1>
+      <button onClick={handleClick}>Get new fact</button>
       {fact && <p className='phrase'>{fact}</p>}
       {imageUrl && <img className='image' src={imageUrl} alt='image extracted by the first word'/>}
     </main> 
